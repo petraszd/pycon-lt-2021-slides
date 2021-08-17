01c9ffc87bd5579a6e9e53bfa438c75e797cef89
Petras Zdanavičius <petraszd@gmail.com>
2021-08-18 00:23:14 +0300

    Using script to preload slides

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -2,35 +2,37 @@ extends Node
 
 const NO_SLIDE = -1
 
-var slides = []
-var current_slide_idx = NO_SLIDE
+var slides = [
+    preload("res://Scenes/Slides/Slide_01.tscn").instance(),
+    preload("res://Scenes/Slides/Slide_02.tscn").instance()
+]
+var current_idx = NO_SLIDE
 
 
 func _ready():
-    slides = $Slides.get_children()
     assert(slides.size() > 0)
     set_current_slide(0)
 
 func _process(_delta):
     if is_asking_for_next_slide():
-        if current_slide_idx < slides.size() - 1:
-            set_current_slide(current_slide_idx + 1)
+        if current_idx < slides.size() - 1:
+            set_current_slide(current_idx + 1)
     elif is_asking_for_prev_slide():
-        if current_slide_idx > 0:
-            set_current_slide(current_slide_idx - 1)
+        if current_idx > 0:
+            set_current_slide(current_idx - 1)
 
 
 func set_current_slide(next_idx):
     var previous_slide = null
-    if current_slide_idx != NO_SLIDE:
-        previous_slide = slides[current_slide_idx]
+    if current_idx != NO_SLIDE:
+        previous_slide = slides[current_idx]
 
-    current_slide_idx = next_idx
-    var next_slide = slides[current_slide_idx]
+    current_idx = next_idx
+    var next_slide = slides[current_idx]
 
     if previous_slide != null:
-        previous_slide.visible = false
-    next_slide.visible = true
+        $CurrentSlidePlace.remove_child(previous_slide)
+    $CurrentSlidePlace.add_child(next_slide)
 
 func is_asking_for_next_slide():
     return (
