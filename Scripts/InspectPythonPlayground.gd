19f76c69fd4d56c8d1e0831baef0a0856ec8f817
Petras Zdanavičius <petraszd@gmail.com>
2021-08-30 10:53:05 +0300

    Preparing for communication with Python

--- Scripts/InspectPythonPlayground.gd
+++ Scripts/InspectPythonPlayground.gd
@@ -1,23 +1,45 @@
 extends HBoxContainer
 
 export var default_color = Color(0, 0, 0)
-export var interesting_color = Color(0, 0, 0)
+export var value_color = Color(0, 0, 0)
+
+
+var refcnt_val = "Unknown"
+var type_val = "Unknown"
+var size_val = "Unknown"
+var digit_vals = []
 
 
 func _ready():
-    var struct = PoolStringArray();
-    var interesting_color_html = interesting_color.to_html(false)
-    struct.append("[color=#%s]{" % default_color.to_html(false))
-    struct.append("  ob_base = {")
-    struct.append("    ob_base = {")
-    struct.append("      ob_refcnt = [color=#%s]TODO[/color];" % interesting_color_html)
-    struct.append("      ob_type   = [color=#%s]PyLong_Type[/color];" % interesting_color_html)
-    struct.append("    };")
-    struct.append("  };")
-    struct.append("  ob_size = [color=#%s]TODO[/color];" % interesting_color_html)
-    struct.append("  ob_digit = [")
-    struct.append("    [color=#%s]TODO[/color]," % interesting_color_html)
-    struct.append("  ];")
-    struct.append("}[/color]")
+    _redraw_int_structure()
 
-    $IntStructure.bbcode_text = struct.join("\n")
+
+func _on_PyTextEditWithRunBtn_after_python_exec(_stdout, stderr):
+    if stderr:
+        $IntStructure.bbcode_text = stderr
+    else:
+        _redraw_int_structure()
+
+
+func _redraw_int_structure():
+    var arr = PoolStringArray();
+    var col = value_color.to_html(false)
+    arr.append("[color=#%s]{" % default_color.to_html(false))
+    arr.append("  ob_base = {")
+    arr.append("    ob_base = {")
+    arr.append("      ob_refcnt = [color=#%s]%s[/color];" % [col, refcnt_val])
+    arr.append("      ob_type   = [color=#%s]%s[/color];" % [col, type_val])
+    arr.append("    };")
+    arr.append("  };")
+    arr.append("  ob_size = [color=#%s]%s[/color];" % [col, size_val])
+
+    if digit_vals.size() == 0:
+        arr.append("  ob_digit = [];")
+    else:
+        arr.append("  ob_digit = [")
+        for digit_val in digit_vals:
+            arr.append("    [color=#%s]%s[/color]," % [col, digit_val])
+        arr.append("  ];")
+    arr.append("}[/color]")
+
+    $IntStructure.bbcode_text = arr.join("\n")
